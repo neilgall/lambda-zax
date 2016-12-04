7ed33d85f475fb0520c496fe7def42d63fbc3cf8
Neil Gall <neilgall@mac.com>
2016-12-04 22:54:05 +0000

    Fix build.sh output directory

--- alexa/build.sh
+++ alexa/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-../AlexaSkillBuilder/.stack-work/install/x86_64-osx/lts-5.18/7.10.3/bin/AlexaSkillBuilder --zfile ../src/main/resources/zork1.dat --preprocessor ./rules.txt --infodump ../../ztools/infodump --output .
+../AlexaSkillBuilder/.stack-work/install/x86_64-osx/lts-5.18/7.10.3/bin/AlexaSkillBuilder --zfile ../src/main/resources/zork1.dat --preprocessor ./rules.txt --infodump ../../ztools/infodump --output out
+
 
 
